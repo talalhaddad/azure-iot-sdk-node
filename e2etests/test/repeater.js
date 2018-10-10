113cfda5dc8f055339197967f96b0ca250b465d9
Anthony Ercolano <anthonyvercolano@outlook.com>
2018-10-09 17:06:20 -0700

    Branch for testing methods.

---
+++ e2etests/test/repeater.js
@@ -0,0 +1,163 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+'use strict';
+var Registry = require('azure-iothub').Registry;
+var ServiceClient = require('azure-iothub').Client;
+var ConnectionString = require('azure-iothub').ConnectionString;
+var deviceSas = require('azure-iot-device').SharedAccessSignature;
+var deviceSdk = require('azure-iot-device');
+var anHourFromNow = require('azure-iot-common').anHourFromNow;
+var uuid = require('uuid');
+var assert = require('chai').assert;
+var debug = require('debug')('e2etests:devicemethod');
+
+var N = 10;
+Array.apply(null, {length: N}).map(Number.call, Number).forEach(function() {
+  [
+    require('azure-iot-device-amqp').Amqp,
+  ].forEach(function(protocolCtor) {
+        describe('Device Methods over ' + protocolCtor.name, function () {
+      this.timeout(120000);
+      var registry = Registry.fromConnectionString(process.env.IOTHUB_CONNECTION_STRING);
+      var deviceClient;
+      var deviceDescription;
+
+      before(function (done) {
+        deviceDescription = {
+          deviceId:  '0000e2etest-delete-me-node-device-method-' + uuid.v4(),
+          status: 'enabled',
+            authentication: {
+            symmetricKey: {
+              primaryKey: new Buffer(uuid.v4()).toString('base64'),
+              secondaryKey: new Buffer(uuid.v4()).toString('base64')
+            }
+          }
+        };
+
+        debug('creating device: ' + deviceDescription.deviceId);
+        registry.create(deviceDescription, function (err) {
+          if (err) {
+            debug('failed to create the device: ' +  deviceDescription.deviceId + ': ' + err.toString());
+            return done(err);
+          } else {
+            debug('created test device: ' + deviceDescription.deviceId);
+            return done();
+          }
+        });
+      });
+
+      after(function (done) {
+        debug('deleting test device: ' + deviceDescription.deviceId);
+        registry.delete(deviceDescription.deviceId, function (err) {
+          if (err) {
+            debug('failed to delete device: ' +  deviceDescription.deviceId + ': ' + err.toString());
+            return done(err);
+          } else {
+            debug('device deleted: ' +  deviceDescription.deviceId);
+            return done();
+          }
+        });
+      });
+
+      // create a new device for every test
+      beforeEach(function (done) {
+        var host = ConnectionString.parse(process.env.IOTHUB_CONNECTION_STRING).HostName;
+        var sas = deviceSas.create(host, deviceDescription.deviceId, deviceDescription.authentication.symmetricKey.primaryKey, anHourFromNow()).toString();
+        deviceClient = deviceSdk.Client.fromSharedAccessSignature(sas, protocolCtor);
+        debug('connecting device client...');
+        deviceClient.open(function (err) {
+          if (err) {
+            debug('error connecting device client: ' + err.toString());
+            return done(err);
+          } else {
+            debug('device client connected');
+            return done();
+          }
+        });
+      });
+
+      // nuke the test device after every test
+      afterEach(function (done) {
+        if (!!deviceClient) {
+          debug('disconnecting device client...');
+          deviceClient.close(function(err) {
+            if (!!err) {
+              console.warn('Could not close connection to device ' + deviceDescription.deviceId + ': ' + err.toString());
+              return done(err);
+            } else {
+              debug('device connection closed');
+              return done();
+            }
+          });
+        } else {
+          debug('no device client. nothing to disconnect');
+          done();
+        }
+      });
+
+      var methodName = 'method1';
+      var methodResult = 200;
+
+      var setMethodHandler = function(testPayload) {
+        debug('---------- new method test ------------');
+        // setup device to handle the method call
+        deviceClient.onDeviceMethod(methodName, function(request, response) {
+          // validate request
+          assert.isNotNull(request);
+          assert.strictEqual(request.methodName, methodName);
+          assert.deepEqual(request.payload, testPayload);
+          debug('device: received method request');
+          debug(JSON.stringify(request, null, 2));
+          // validate response
+          assert.isNotNull(response);
+
+          // send the response
+          response.send(methodResult, testPayload, function(err) {
+            debug('send method response with statusCode: ' + methodResult);
+            if(!!err) {
+              console.error('An error ocurred when sending a method response:\n' +
+                  err.toString());
+            }
+          });
+        });
+      };
+
+      var sendMethodCall = function(serviceClient, deviceId, testPayload, done) {
+        setTimeout(function() {
+          // make the method call via the service
+          var methodParams = {
+            methodName: methodName,
+            payload: testPayload,
+            timeoutInSeconds: 10
+          };
+          debug('service sending method call:');
+          debug(JSON.stringify(methodParams, null, 2));
+          serviceClient.invokeDeviceMethod(
+                  deviceId,
+                  methodParams,
+                  function(err, result) {
+                    if(!err) {
+                      debug('got method results');
+                      debug(JSON.stringify(result, null, 2));
+                      assert.strictEqual(result.status, methodResult);
+                      assert.deepEqual(result.payload, testPayload);
+                    } else {
+                      debug('--------error returned from invoke');
+                    }
+                    debug('---------- end method test ------------');
+                    done(err);
+                  });
+        }, 1000);
+      };
+
+      [null].forEach(function(testPayload) {
+        it('makes and receives a method call with ' + JSON.stringify(testPayload), function(done) {
+          setMethodHandler(testPayload);
+          var serviceClient = ServiceClient.fromConnectionString(process.env.IOTHUB_CONNECTION_STRING);
+          sendMethodCall(serviceClient, deviceDescription.deviceId, testPayload, done);
+        });
+      });
+    });
+  });
+});
